df78ae6881afadacb59c46f3597aabbd3c641f89
Rodrigo Martins <42254509+rodrigoomartins@users.noreply.github.com>
2024-10-02 11:18:40 -0300

    Add files via upload

---
+++ rfdash.py
@@ -0,0 +1,398 @@
+import streamlit as st
+import pandas as pd
+import csv
+import io
+from st_aggrid import AgGrid, GridOptionsBuilder, GridUpdateMode, DataReturnMode
+from datetime import datetime
+import unidecode
+import pdfkit
+import plotly.express as px
+import tempfile
+import json
+import os
+import numpy as np
+from streamlit import session_state as state
+
+# Configurações padrão do Streamlit
+st.set_page_config(layout="wide", page_title="Análise de Divergência")
+
+logo_claro_path = "logo_claro_votu.png"
+logo_icon_claro_path = "logo_icon_claro.png"
+logo_escuro_path = "logo_escuro_votu.png"
+site_url = "https://www.voturfid.com.br"
+estoque_esperado_exemplo_path = "POSICAO_ESTOQUE_DPA_20240630.csv"
+contagem_rfid_exemplo_path = "Inventário-Ordem_271.csv"
+#st.image (logo_claro_path, width=150)
+
+
+# Adiciona a imagem sem a opção de expandir
+st.logo(image=logo_claro_path,icon_image=logo_icon_claro_path,link=site_url)
+with st.sidebar:
+        st.header("Esta é uma ferramenta para análise de divergência de inventários feitos utilizando a tecnologia de RFID da Votu.")
+        st.divider()
+        st.write(f"São necessários dois arquivos para gerar a análise:\n- CSV do estoque esperado;\n- CSV da contagem do inventário com RFID.\n\nO arquivo CSV do estoque esperado, deve conter as seguintes informações:\n- EAN\n- PRODUTO\n- REFERENCIA\n- DESCRICAO\n- COR\n- TAMANHO\n- ESTOQUE\n- LOCALIZADOR\n\nO arquivo CSV da contagem com RFID é gerado pelo RFLOG e contém EAN e Quantidade dos produtos lidos.\n\nSeguem dois arquivos de exemplo:\n\n")
+        st.download_button(label="CSV estoque esperado",data=estoque_esperado_exemplo_path,file_name="POSICAO_ESTOQUE.csv",mime="text/csv")
+        st.download_button(label="CSV inventário RFID",data=contagem_rfid_exemplo_path,file_name="CONTAGEM_RFID.csv",mime="text/csv")
+        st.write("É possível carregar mais de um arquivo CSV de inventário e escolher qual será comparado com o estoque esperado.\nA tabela de divergência permite vários tipos de filtragens, ordenações e outras configurações disponíveis.\nAo fim, é possível gerar um arquivo PDF da tabela de divergência.")
+        
+        
+# Função para detectar o delimitador do CSV
+def detect_delimiter(file):
+    try:
+        sample = file.read(1024).decode('utf-8')
+        file.seek(0)  # Resetar o ponteiro do arquivo para o início
+        sniffer = csv.Sniffer()
+        dialect = sniffer.sniff(sample)
+        return dialect.delimiter
+    except Exception as e:
+        st.warning(f"Não foi possível detectar o delimitador automaticamente. Usando ',' como padrão.")
+        return ','  # Padrão para vírgula
+
+# Função para normalizar nomes de colunas
+def normalize_column_names(columns):
+    return [unidecode.unidecode(col).upper().replace(' ', '_') for col in columns]
+
+# Função para salvar métricas
+def save_metrics(metrics, filename="metrics.json"):
+    data = []
+    if os.path.exists(filename):
+        try:
+            with open(filename, 'r') as f:
+                data = json.load(f)
+        except (json.JSONDecodeError, FileNotFoundError):
+            st.error("Erro ao carregar o arquivo de métricas. Inicializando um novo arquivo.")
+            data = []
+
+    # Convertendo os valores do dicionário metrics para tipos de dados do Python
+    converted_metrics = {k: (int(v) if isinstance(v, (np.integer, int)) else float(v) if isinstance(v, (np.floating, float)) else v) for k, v in metrics.items()}
+    
+    data.append(converted_metrics)
+
+    with open(filename, 'w') as f:
+        json.dump(data, f, indent=4)
+
+# Função para carregar os dados do CSV
+def load_data(file):
+    delimiter = detect_delimiter(file)
+    try:
+        file.seek(0)
+        # Lendo o CSV com o quotechar configurado para lidar com vírgulas internas
+        data = pd.read_csv(file, sep=delimiter, quotechar='"', engine='python', on_bad_lines='skip')
+    except pd.errors.ParserError as e:
+        st.error(f"Erro ao ler o arquivo CSV: {e}")
+        return pd.DataFrame()  # Retorna um DataFrame vazio em caso de erro
+
+    # Renomear colunas para formato normalizado
+    data.columns = normalize_column_names(data.columns)
+    
+    # Renomear coluna QUANTIDADE para CONTAGEM
+    if 'QUANTIDADE' in data.columns:
+        data.rename(columns={'QUANTIDADE': 'CONTAGEM'}, inplace=True)
+    
+    return data
+
+# Função para calcular divergências
+def calculate_discrepancies(expected, counted, file_name):
+    # Verificar se a coluna 'EAN' existe em ambos os DataFrames
+    if 'EAN' not in expected.columns or 'EAN' not in counted.columns:
+        st.error("A coluna 'EAN' não foi encontrada em um dos arquivos CSV.")
+        return pd.DataFrame()  # Retorna um DataFrame vazio em caso de erro
+
+    # Converte a coluna 'EAN' para string em ambos os DataFrames
+    expected['EAN'] = expected['EAN'].astype(str)
+    counted['EAN'] = counted['EAN'].astype(str)
+
+    # Adiciona a coluna 'ESTOQUE' com valor 0 no expected se não existir
+    if 'ESTOQUE' not in expected.columns:
+        expected['ESTOQUE'] = 0
+
+    # Adiciona a coluna 'CONTAGEM' com valor 0 no counted se não existir
+    if 'CONTAGEM' not in counted.columns:
+        counted['CONTAGEM'] = 0
+
+    # Merge completo dos dados esperados e contados usando EAN como chave
+    discrepancies = pd.merge(expected, counted, on='EAN', how='outer', suffixes=('_EXPECTED', '_COUNTED'))
+
+    # Substitui NaN em 'ESTOQUE' e 'CONTAGEM' por 0
+    discrepancies['ESTOQUE'].fillna(0, inplace=True)
+    discrepancies['CONTAGEM'].fillna(0, inplace=True)
+
+    # Cálculo da divergência
+    discrepancies['DIVERGÊNCIA'] = discrepancies['CONTAGEM'] - discrepancies['ESTOQUE']
+
+    # Adicionar coluna para reler peças apenas para os SKUs divergentes
+    discrepancies['PEÇAS A SEREM RELIDAS'] = discrepancies.apply(lambda row: max(row['ESTOQUE'], row['CONTAGEM']) if row['DIVERGÊNCIA'] != 0 else 0, axis=1)
+
+    # Arredondar números para inteiros
+    discrepancies = discrepancies.astype({'ESTOQUE': int, 'CONTAGEM': int, 'DIVERGÊNCIA': int})
+
+    # Adicionar coluna com o nome do arquivo de contagem
+    #discrepancies['ARQUIVO_DE_CONTAGEM'] = file_name
+
+    return discrepancies
+
+def convert_df_to_csv(df):
+    # Colunas a serem convertidas para string
+    column_list = ['PRODUTO', 'REFERENCIA']
+
+    # Itera sobre as colunas na lista
+    for column in column_list:
+        if column in df.columns:
+            df[column] = df[column].astype(str)
+
+    return df.to_csv(index=False, encoding='utf-8-sig').encode('utf-8-sig')
+
+
+# Função para gerar um timestamp
+def generate_timestamp():
+    return datetime.now().strftime("%Y%m%d_%H%M")
+
+# Função para gerar o PDF da tabela filtrada
+def generate_pdf(filtered_df, font_family, font_size, orientation):
+    # Estilos CSS para formatação do PDF (com opções do usuário e orientação paisagem forçada)
+    # Calcular o resumo dinâmico do inventário
+    if not filtered_df.empty:
+        total_estoque = filtered_df['ESTOQUE'].sum()
+        total_contagem = filtered_df['CONTAGEM'].sum()
+        total_divergencia_positiva = filtered_df[filtered_df['DIVERGÊNCIA'] > 0]['DIVERGÊNCIA'].sum()
+        total_divergencia_negativa = filtered_df[filtered_df['DIVERGÊNCIA'] < 0]['DIVERGÊNCIA'].sum()
+        total_divergencia_absoluta = filtered_df['DIVERGÊNCIA'].abs().sum()
+    else:
+        total_estoque = 0
+        total_contagem = 0
+        total_divergencia_positiva = 0
+        total_divergencia_negativa = 0
+        total_divergencia_absoluta = 0
+    
+    css = f"""
+    <style>
+    table {{
+        width: 29.7cm; /* Largura A4 paisagem */
+        border-collapse: collapse;
+        font-family: {font_family};
+        font-size: {font_size}px;
+        page-break-inside: avoid;
+        letter-spacing: 1px;
+    }}
+    th, td {{
+        border: 1px solid black;
+        padding: 8px;
+        white-space: nowrap; /* Evita quebra de linha nas células */
+    }}
+    tr:nth-child(even) {{
+        background-color: #f2f2f2; /* Cor de fundo para linhas pares */
+    }}
+    @page {{
+        size: A4 landscape;
+        margin: 1cm;
+    }}
+    @page {{ 
+        @bottom-center {{
+            content: element(footer); }}
+        }}
+    thead {{
+        display: table-header-group; /* Repetir cabeçalho em cada página */
+    }}
+    </style>
+    """
+
+    # Converter a coluna "PRODUTO" para inteiro, se existir, e remover valores não numéricos
+    if 'PRODUTO' in filtered_df.columns:
+        filtered_df['PRODUTO'] = pd.to_numeric(filtered_df['PRODUTO'], errors='coerce').fillna(0).astype(int)
+
+    # Converter a coluna "REFERENCIA" para inteiro, se existir, e remover valores não numéricos
+    if 'REFERENCIA' in filtered_df.columns:
+        filtered_df['REFERENCIA'] = pd.to_numeric(filtered_df['REFERENCIA'], errors='coerce').fillna(0).astype(int)
+
+    # Criar tabela HTML para o resumo
+    summary_html = f"""
+    <table style="margin-top: 20px; width: 50%;">
+        <thead>
+            <tr>
+                <th><b>RESUMO</b></th>
+                <td><b>TOTAIS</b></td>
+            </tr>    
+            <tr>
+                <th>Total Esperado em Estoque</th>
+                <td><b>{total_estoque}</b></td>
+            </tr>
+            <tr>
+                <th>Total da Contagem do Inventário</th>
+                <td><b>{total_contagem}<b></td>
+            </tr>
+            <tr>
+                <th>Total de Divergência Positiva (Sobrando)</th>
+                <td><b>{total_divergencia_positiva}</b></td>
+            </tr>
+            <tr>
+                <th>Total de Divergência Negativa (Faltando)</th>
+                <td><b>{total_divergencia_negativa}</b></td>
+            </tr>
+            <tr>
+                <th>Total de Divergência Absoluta</th>
+                <td><b>{total_divergencia_absoluta}</b></td>
+            </tr>
+        </thead>
+    </table>
+    """
+
+    # Corrigir nomes de colunas do DataFrame para incluir espaços em vez de underscores
+    filtered_df.columns = filtered_df.columns.str.replace('_', ' ')
+
+    # Tabela HTML dos dados filtrados
+    table_html = filtered_df.to_html(index=False, justify='left')
+
+    # HTML completo
+    html_content = f"""
+    <html>
+    <head>
+    {css}
+    </head>
+    <body>
+        {table_html}
+        <footer style="text-align: center; font-size: 12px; padding: 10px;">
+            <p>Visite nosso site: <a href="https://www.voturfid.com.br" target="_blank">www.voturfid.com.br</a></p>
+        </footer>
+    </body>
+    </html>
+    """
+
+    # Configuração do pdfkit com orientação e numeração de páginas
+    options = {
+        'page-size': 'A4',
+        'orientation': f'{orientation}',
+        'margin-top': '1cm',
+        'margin-right': '1cm',
+        'margin-bottom': '1.5cm',
+        'margin-left': '1cm',
+        'encoding': "UTF-8",
+        'footer-right': '[page] de [topage]',
+    }
+
+    # Gerar o PDF a partir do HTML
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".html") as temp_html_file:
+        temp_html_file.write(html_content.encode('utf-8'))
+        temp_html_path = temp_html_file.name
+
+    pdf_path = temp_html_path.replace(".html", ".pdf")
+    pdfkit.from_file(temp_html_path, pdf_path, options=options)
+
+    return pdf_path
+
+# Função para gerar gráfico de pizza
+def generate_pie_chart(total_contagem, total_divergencia_absoluta, total_estoque):
+    accuracy = (total_contagem - total_divergencia_absoluta) / total_estoque * 100
+    labels = ['Acurácia', 'Inacurácia']
+    values = [accuracy, 100 - accuracy]
+    fig = px.pie(values=values, names=labels, title='Acurácia do Inventário')
+    return fig
+
+# Função para exibir tabela de dados
+def display_data_table(df):
+    gb = GridOptionsBuilder.from_dataframe(df)
+    gb.configure_pagination(enabled=False)  # Desativar paginação
+    gb.configure_side_bar(True)
+    gb.configure_default_column(value=True, enableRowGroup=True, aggFunc='sum', editable=True, groupable=True, filter=True, sortable=True)
+    gb.configure_grid_options(domLayout='normal', enableEnterpriseModules = True)  # Configurar altura automática para rolagem infinita
+    grid_options = gb.build()
+
+    grid_response = AgGrid(
+        df,
+        gridOptions=grid_options,
+        data_return_mode=DataReturnMode.FILTERED_AND_SORTED,
+        update_mode=GridUpdateMode.MODEL_CHANGED,
+        enable_enterprise_modules=True,
+        height=750,
+        reload_data=True
+    )
+
+    filtered_df = pd.DataFrame(grid_response['data'])
+    return filtered_df
+
+# Título da aplicação
+st.title("Análise de Divergência")
+
+# Upload de arquivos esperados e contados
+expected_file = st.file_uploader("Carregar arquivo CSV com estoque esperado", type=['csv'], key="expected")
+counted_files = st.file_uploader("Carregar arquivos CSV do inventário RFID", type=['csv'], accept_multiple_files=True, key="counted")
+
+# Função para mostrar o resumo de inventário de forma estilizada
+def show_summary(discrepancies):
+    total_estoque = discrepancies['ESTOQUE'].sum()
+
+    st.divider()
+    st.metric('Total Esperado em Estoque', total_estoque)
+
+# Dicionário para armazenar divergências de múltiplos arquivos
+all_discrepancies = {}
+
+# Processar os arquivos carregados
+if expected_file is not None and counted_files is not None:
+    expected_df = load_data(expected_file)
+    for counted_file in counted_files:
+        counted_df = load_data(counted_file)
+        file_name = counted_file.name  # Nome do arquivo de contagem
+        discrepancies = calculate_discrepancies(expected_df, counted_df, file_name)
+        all_discrepancies[file_name] = discrepancies
+
+    # Exibir filtro para selecionar arquivos
+    selected_file = st.selectbox("Selecione o arquivo de contagem para visualizar", options=list(all_discrepancies.keys()))
+
+    
+    # Exibir tabela de dados filtrados para o arquivo selecionado
+    if selected_file:
+        filtered_df = display_data_table(all_discrepancies[selected_file])
+        
+        show_summary(discrepancies)
+
+        # Exibir métricas do resumo dinâmico
+        if not filtered_df.empty:
+            total_estoque = int(filtered_df['ESTOQUE'].sum())
+            total_contagem = int(filtered_df['CONTAGEM'].sum())
+            total_divergencia_positiva = int(filtered_df[filtered_df['DIVERGÊNCIA'] > 0]['DIVERGÊNCIA'].sum())
+            total_divergencia_negativa = int(filtered_df[filtered_df['DIVERGÊNCIA'] < 0]['DIVERGÊNCIA'].sum())
+            total_divergencia_absoluta = int(filtered_df['DIVERGÊNCIA'].abs().sum())
+            total_pecas_a_serem_relidas = discrepancies[discrepancies['DIVERGÊNCIA'] != 0]['PEÇAS A SEREM RELIDAS'].sum()
+            total_estoque_global = discrepancies['ESTOQUE'].sum()
+            total_contagem_global = discrepancies['CONTAGEM'].sum()
+            st.subheader("Resumo Dinâmico")
+            st.caption("Valores filtrados")
+            st.metric("Estoque Esperado", total_estoque)
+            st.metric("Contagem do Inventário", total_contagem, delta=f"{(total_contagem-total_divergencia_absoluta)/total_estoque*100:.2f}% (acurácia)")
+            
+            col1, col2, col3, col4 = st.columns(4)
+            with col1:    
+                st.metric("Sobra", total_divergencia_positiva, delta=f"{(total_divergencia_positiva/total_contagem)*100:.2f}%", delta_color='inverse')
+            with col2:
+                st.metric("Falta", total_divergencia_negativa, delta=f"{(total_divergencia_negativa/total_contagem)*-100:.2f}%",delta_color='inverse')
+            with col3:
+                st.metric("Divergência Absoluta", total_divergencia_absoluta, delta=f"{(total_divergencia_absoluta/total_contagem)*100:.2f}%", delta_color='inverse')
+            with col4:
+                st.metric("Total de peças a serem relidas", f"{total_pecas_a_serem_relidas:.0f}", delta=f"{(total_pecas_a_serem_relidas/total_contagem_global)*100:.2f}%", delta_color='inverse')
+            # Salvar métricas no arquivo JSON
+            metrics = {
+                'total_estoque': total_estoque,
+                'total_contagem': total_contagem,
+                'total_divergencia_positiva': total_divergencia_positiva,
+                'total_divergencia_negativa': total_divergencia_negativa,
+                'total_divergencia_absoluta': total_divergencia_absoluta,
+                'timestamp': generate_timestamp(),
+                'nome_arquivo_contagem': selected_file
+            }
+            save_metrics(metrics)
+
+        # Gerar gráfico de pizza para acurácia
+        st.divider()
+        fig_pie_chart = generate_pie_chart(total_contagem, total_divergencia_absoluta, total_estoque)
+        st.plotly_chart(fig_pie_chart)
+
+        with st.expander("Gerar PDF"):
+            font_family = st.selectbox("Família da Fonte", ["Arial", "Courier", "Helvetica", "Times New Roman", "Verdana"], index=0)
+            font_size = st.slider("Tamanho da Fonte", min_value=6, max_value=20, value=10)
+            orientation = st.selectbox("Orientação", ["Landscape", "Portrait"], index=0)  # Paisagem como padrão
+
+            # Botão para gerar PDF
+            if st.button("Gerar PDF"):
+                pdf_path = generate_pdf(filtered_df, font_family, font_size, orientation)
+                with open(pdf_path, "rb") as pdf_file:
+                    st.download_button(label="Baixar PDF", data=pdf_file, file_name=f"resumo_divergencia_{generate_timestamp()}.pdf")
